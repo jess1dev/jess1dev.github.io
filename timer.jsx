82b4f9515c6e97f6efeb68c32eebbd257d2da31e
jess1dev <79570545+jess1dev@users.noreply.github.com>
2021-03-13 13:50:59 +0000

    Update timer.jsx

--- timer.jsx
+++ timer.jsx
@@ -1,5 +1,6 @@
 let timeS = "0.00";
 let time = 1.0;
+let prvTimer = 0.0;
 let counting = false;
 let waiting = false;
 
@@ -8,13 +9,37 @@ function tick() {
 
     const timer = <span class="time text-light align-middle">{timeS}</span>;
     ReactDOM.render(timer, document.getElementById('time'));
-
+    if(counting)
+    {
+        time += 0.05;
+    }
   }
   
-  setInterval(tick, 1000);
+  setInterval(tick, 50);
 
   document.addEventListener('keydown', function(event) {
     if(event.key == " ") {
         alert('Space was pressed');
+        if(prvTimer < 1.0)
+        {
+            waiting = true;
+        }
+        else
+        {
+            waiting = false;
+        }
+    }
+});
+
+
+document.addEventListener('keyup', function(event) {
+    if(event.key == " ") {
+        alert('Space was released');
+        if(waiting)
+        {
+            prvTimer = 0;
+            waiting = false;
+            counting = true;
+        }
     }
 });
