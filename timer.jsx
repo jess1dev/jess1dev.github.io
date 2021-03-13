57e59da57c398ee3c65e70927bf60a4fefcf7151
jess1dev <79570545+jess1dev@users.noreply.github.com>
2021-03-13 13:45:40 +0000

    Update timer.jsx

--- timer.jsx
+++ timer.jsx
@@ -1,8 +1,8 @@
 let timeS = "0.00";
-let time = 0.0;
+let time = 1.0;
 
 function tick() {
-    timeS = (Math.round(time * 100) / 100).toFixed(2);;
+    timeS = time.toFixed(2);;
 
     const timer = <span class="time text-light align-middle">{timeS}</span>;
     ReactDOM.render(timer, document.getElementById('time'));
