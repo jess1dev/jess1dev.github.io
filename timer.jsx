c7dc313fd181f8dd4af352437434771fa2866d44
jess1dev <79570545+jess1dev@users.noreply.github.com>
2021-03-13 13:39:36 +0000

    Update to time

--- timer.jsx
+++ timer.jsx
@@ -2,8 +2,3 @@ let time = "0.10";
 const timer = <span class="time text-light align-middle">{time}</span>;
 
 ReactDOM.render(timer, document.getElementById('time'));
-
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
